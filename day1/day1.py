b7ed9e500c85826c582e481ec9ad6ccc3b18c3c8
codinglikeagirl42 <codinglikeagirl42@gmail.com>
2023-12-05 01:40:14 -0600

    Day 1 and 2 AOC2023

--- day1/day1.py
+++ day1/day1.py
@@ -4,11 +4,33 @@ callabration_sum = 0
 
 for line in data:
     callibration = []
+    
     for l in line:
         if l.isnumeric() == True:
             callibration.append(l)
     #print(callibration)
     callibration_number = int(callibration[0] + callibration[-1])
     callabration_sum += callibration_number
-print("Part 1:" + callabration_sum)
- 
+print("Part 1: " + str(callabration_sum))
+
+# For part 2 used the enumerate function in python.
+# More inforamtion can be found here: https://www.geeksforgeeks.org/enumerate-in-python/
+# creat a list of written numbers
+written_numbers = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+callibration_sum_two = 0
+for line in data:
+    callibration_two = []
+    # loop thru all characters in each line by i(index)
+    for i, char in enumerate(line):
+        # loop thru written_numbers by index value(value)
+        for value, number in enumerate(written_numbers):
+            # if a written number is found add its value to our list
+            if number in line[i:i+len(number)]:
+                # add 1 to the index(value) to account for no zero
+                #alternitivly you could add zero to the list
+                callibration_two.append(str(value + 1))
+        if char.isnumeric() == True:
+            callibration_two.append(char)
+    callibration_number_two = int(callibration_two[0] + callibration_two[-1])
+    callibration_sum_two += callibration_number_two
+print("Part 2: " + str(callibration_sum_two))
---
+++ day2/day2.py
@@ -0,0 +1,36 @@
+#Get data from text file
+data = open("day2/day2.txt", "r").read().split("\n")
+
+sum = 0
+sum_min = 0 
+
+for i, game in enumerate(data):
+    game = game.split(":")[1].split(";")
+    #print(game)
+    #create list to hold and check against values red, green, blue
+    rgb = [0, 0, 0]
+    game_possible = True
+    for cubes_shown in game:
+        cubes_shown = cubes_shown.strip( ).split(",")
+        #print(cubes_shown)
+        for cubes in cubes_shown:
+            cubes = cubes.strip().split(" ")
+            #print(cubes)
+
+            if (((cubes[1]) == "red" and int(cubes[0]) > 12) or 
+                ((cubes[1]) == "green" and int(cubes[0]) > 13) or 
+                ((cubes[1]) == "blue" and int(cubes[0]) > 14)):
+                game_possible = False
+            if cubes[1] == "red" and int(cubes[0]) > rgb[0]:
+                rgb[0] = int(cubes[0])
+            if cubes[1] == "green" and int(cubes[0]) > rgb[1]:
+                rgb[1] = int(cubes[0])
+            if cubes[1] == "blue" and int(cubes[0]) > rgb[2]:
+                rgb[2] = int(cubes[0])
+    if game_possible:
+        sum += (i + 1)
+    sum_min += (rgb[0] * rgb[1] * rgb[2])
+
+print("Part 1: " + str(sum))
+print("Part 2: " + str(sum_min))
+
