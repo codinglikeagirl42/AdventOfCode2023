506a1c5c64905eeeefe93285c55d2033a1e51289
codinglikeagirl42 <codinglikeagirl42@gmail.com>
2023-12-07 15:03:33 -0600

    Original code for day 3 that does work

--- day3/day3_p2.py
+++ day3/day3_p2.py
@@ -4,7 +4,7 @@ data = open("day3/day3.txt", "r").read().split("\n")
 my_number = ""
 sum = 0
 
-special_char = "[@_!#$%^&*()<>?/\|}{~:+-]"
+#special_char = "[@_!#$%^&*()<>?/\|}{~:+-]"
 
 # I hate mapping pulling from last year
 # Break statements - https://www.geeksforgeeks.org/python-break-statement/
@@ -33,23 +33,19 @@ for x in range(0, x_total):
                 y_end = min(y_total - 1, y)
                 x_start = max(0, x - 1)
                 x_end = min(x_total - 1, x + 1)
-                # Had a ton of issues but my original code does work without adding in breaks.
-                # Note to kill terminal when trying to figure things out.
-                #done = False
+                
+                done = False
                 for yy in range(y_start, y_end + 1):
                     for xx in range(x_start, x_end + 1):
-                        
+                        # must also check for .
                         if data[xx][yy] in sympols:
                             sum += int(my_number)
                             #print(my_number)
                             my_number = ""
                             done = True
-                            #break
-                    #if done == True:
-                        #break
+                            break
+                    if done == True:
+                        break
                 my_number = ""
 
-print("Part 1: " + str(sum))
-
-
-
+print("Part 2: " + str(sum))
